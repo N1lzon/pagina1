58cc8e713e4131d1ed90b83d06ec7954bff7d3ad
Th4mx <130414356+Th4mx@users.noreply.github.com>
2023-04-10 20:00:32 -0400

    Add files via upload

--- js/tiempo.js
+++ js/tiempo.js
@@ -1,5 +1,5 @@
 function check(){ 
-    let url = "https://api.openweathermap.org/data/2.5/weather?lat=-27.2961258&lon=-55.8972359&units=metric&appid=cd27065178e75a759a20202fb2deebda"
+    let url = "https://api.openweathermap.org/data/2.5/weather?lat=-27.2961258&lon=-55.8972359&units=metric&appid=cd27065178e75a759a20202fb2deebda&lang=es"
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
